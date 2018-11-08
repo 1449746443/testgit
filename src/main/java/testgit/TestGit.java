90de5a586a26e6dd1d75e50ec3c588c233003d58
eclipse_user <eclipse_user@163.com>
2018-11-08 10:25:49 +0800

    调整方法逻辑

--- src/main/java/testgit/TestGit.java
+++ src/main/java/testgit/TestGit.java
@@ -5,6 +5,9 @@ public class TestGit {
 	public static void main(String[] args) {
 		System.out.println("main12 function-----");
 
+		System.out.println("main13 function-----");
+		
+		System.out.println("main14 function-----");
 	}
 	
 
